80ed292bc8cfa784219502fdb7309ec9e049e3fb
Владислав <shykovskiy@gmail.com>
2021-02-28 16:05:30 +0200

    Start template with react-bootstrap, axios, react-router and sass

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+export const App = () => {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+import {App} from './components/App/App';
+
+
+render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+

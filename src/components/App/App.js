1663ca713763abfb9ed206b9d2525ea1dccace80
Владислав <shykovskiy@gmail.com>
2021-03-03 08:27:46 +0200

    Update App: added router navigation, import pages and update styles for .App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,22 @@
+import React from 'react'
 import './App.scss';
 
+import {Route, Switch} from "react-router-dom";
+import {CatPage} from "../Pages/CatPage";
+import {MainPage} from "../Pages/MainPage";
+
 export const App = () => {
+
     return (
         <div className="App">
-
+            <Switch>
+                <Route path='/' exact>
+                    <MainPage/>
+                </Route>
+                <Route path={`/:topicId`}>
+                    <CatPage/>
+                </Route>
+            </Switch>
         </div>
     );
 }

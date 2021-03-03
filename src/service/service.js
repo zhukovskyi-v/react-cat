01d6d732d606c6aa8cfd03b57e7049d71dec74d4
Владислав <shykovskiy@gmail.com>
2021-03-03 08:25:40 +0200

    Added service with axios

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const API = `d30e9655-d5b7-413a-a45e-bfa6e6fafe25`;
+
+// const API = `d30e9655-d5b7-413a-a45e-bfa6e6fafe25`;
 
 export const axiosInstance = axios.create({
-    baseURL: `https://api.thecatapi.com/`
+    baseURL: `https://api.thecatapi.com/v1/`
 });
